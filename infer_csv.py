66ebf948fbb4ecf0f10fd08729f8985b380d8c85
gitGut01 <sile@computas.com>
2024-10-11 21:04:33 +0200

    Get single df

--- infer_csv.py
+++ infer_csv.py
@@ -16,11 +16,7 @@ COLOR_HORIZONTAL = (255, 0, 0)  # Color for horizontal lines
 COLOR_VERTICAL = (0, 255, 0)    # Color for vertical lines
 COLOR_INTERSECTION = (0, 0, 255) # Color for intersections
 COLOR_START = (0, 255, 255) 
-COLOR_END = (0, 255, 255) 
-
-
-INPUT_IMAGE = 'demo/image.png'
-OUTPUT_IMAGE = 'demo/output_image_with_intersections.png'
+COLOR_END = (0, 255, 255)
 
 
 def read_images(image_path):
@@ -36,11 +32,11 @@ def get_binary_map(gray_image):
     return binary_map
 
 
-def get_horizontal_lines(image, binary_map, line_thickness, line_shift, min_distance, min_line_length, color):
+def get_horizontal_lines(binary_map):
     """Get horizontal lines on the image based on the binary map."""
     height, width = binary_map.shape
     horizontal_lines = []  # Stores y-coordinates of horizontal lines
-    last_y = -min_distance  # Initialize to a value smaller than any possible y-coordinate
+    last_y = -MIN_DISTANCE  # Initialize to a value smaller than any possible y-coordinate
 
     for y in range(height):
         count = 0  # Count consecutive white pixels
@@ -48,26 +44,26 @@ def get_horizontal_lines(image, binary_map, line_thickness, line_shift, min_dist
             if binary_map[y, x] == 255:  # White pixel
                 count += 1
             else:
-                if count > min_line_length and y - last_y >= min_distance:
-                    if y + line_shift < height:  # Check bounds
-                        horizontal_lines.append(y + line_shift)
+                if count > MIN_LINE_LENGTH and y - last_y >= MIN_DISTANCE:
+                    if y + LINE_SHIFT < height:  # Check bounds
+                        horizontal_lines.append(y + LINE_SHIFT)
                         last_y = y
                 count = 0  # Reset count if the sequence is broken
 
         # Check if the line reaches the end of the row
-        if count > min_line_length and y - last_y >= min_distance:
-            if y + line_shift < height:
-                horizontal_lines.append(y + line_shift)
+        if count > MIN_LINE_LENGTH and y - last_y >= MIN_DISTANCE:
+            if y + LINE_SHIFT < height:
+                horizontal_lines.append(y + LINE_SHIFT)
                 last_y = y
 
     return horizontal_lines
 
 
-def get_vertical_lines(image, binary_map, line_thickness, line_shift, min_distance, min_line_length, color):
+def get_vertical_lines(binary_map):
     """Get vertical lines on the image based on the binary map."""
     height, width = binary_map.shape
     vertical_lines = []  # Stores x-coordinates of vertical lines
-    last_x = -min_distance  # Initialize to a value smaller than any possible x-coordinate
+    last_x = -MIN_DISTANCE  # Initialize to a value smaller than any possible x-coordinate
 
     for x in range(width):
         count = 0  # Count consecutive white pixels
@@ -75,60 +71,55 @@ def get_vertical_lines(image, binary_map, line_thickness, line_shift, min_distan
             if binary_map[y, x] == 255:  # White pixel
                 count += 1
             else:
-                if count > min_line_length and x - last_x >= min_distance:
-                    if x + line_shift < width:  # Check bounds
-                        vertical_lines.append(x + line_shift)
+                if count > MIN_LINE_LENGTH and x - last_x >= MIN_DISTANCE:
+                    if x + LINE_SHIFT < width:  # Check bounds
+                        vertical_lines.append(x + LINE_SHIFT)
                         last_x = x
                 count = 0  # Reset count if the sequence is broken
 
         # Check if the line reaches the end of the column
-        if count > min_line_length and x - last_x >= min_distance:
-            if x + line_shift < width:
-                vertical_lines.append(x + line_shift)
+        if count > MIN_LINE_LENGTH and x - last_x >= MIN_DISTANCE:
+            if x + LINE_SHIFT < width:
+                vertical_lines.append(x + LINE_SHIFT)
                 last_x = x
 
     return vertical_lines
 
 
-def mark_intersections(image, horizontal_lines, vertical_lines, color):
+def get_intersections(horizontal_lines, vertical_lines):
     """Get horizontal and vertical lines"""
     intersection_coordinates = []
     for i in range(min(len(horizontal_lines), len(vertical_lines))):
         intersection_point = (vertical_lines[i], horizontal_lines[i])  # (x, y) coordinate of the intersection
         intersection_coordinates.append(intersection_point)
 
-        print(f'Intersection {i + 1}: (X: {intersection_point[0]}, Y: {intersection_point[1]})')
-
     return intersection_coordinates
 
 
-def draw_vertical_line_at_leftmost_white(image, binary_map, blue_line_y):
+def get_most_top_left_white_pixel(binary_map, first_y_line):
     """Draw a vertical line at the furthest left white pixel within a thickness of 10 pixels."""
     height, width = binary_map.shape
     leftmost_x = None
 
     # Search for the leftmost white pixel within SEARCH_THICKNESS above and below the blue line
-    for y in range(max(0, blue_line_y - SEARCH_THICKNESS), min(height, blue_line_y + SEARCH_THICKNESS)):
+    for y in range(max(0, first_y_line - SEARCH_THICKNESS), min(height, first_y_line + SEARCH_THICKNESS)):
         for x in range(width):
             if binary_map[y, x] == 255:  # Found a white pixel
                 if leftmost_x is None or x < leftmost_x:
                     leftmost_x = x
                 break  # Stop searching this row after finding the first white pixel
 
-    # Draw a vertical line if a leftmost white pixel was found
     if leftmost_x is not None:
-        # cv2.line(image, (leftmost_x, 0), (leftmost_x, height - 1), COLOR_YELLOW, LINE_THICKNESS)
-        print(f'Drawing vertical line at X: {leftmost_x}')
-        return (leftmost_x, blue_line_y)
+        return (leftmost_x, first_y_line)
 
 
-def draw_horizontal_line_at_bottom_most_white(image, binary_map, green_line_x):
+def get_most_bottom_right_white_pixel(binary_map, last_x_line):
     """Draw a horizontal line at the furthest bottom white pixel within a thickness of 10 pixels."""
     height, width = binary_map.shape
     bottom_most_y = None
 
     # Search for the bottom-most white pixel within SEARCH_THICKNESS above and below the green line
-    for x in range(max(0, green_line_x - SEARCH_THICKNESS), min(height, green_line_x + SEARCH_THICKNESS)):
+    for x in range(max(0, last_x_line - SEARCH_THICKNESS), min(height, last_x_line + SEARCH_THICKNESS)):
         # for y in range(height):
         for y in range(height - 1, -1, -1):
             if binary_map[y, x] == 255:  # Found a white pixel
@@ -136,24 +127,21 @@ def draw_horizontal_line_at_bottom_most_white(image, binary_map, green_line_x):
                     bottom_most_y = y
                 break  # Stop searching this row after finding the first white pixel
 
-    # Draw a horizontal line if a bottom-most white pixel was found
     if bottom_most_y is not None:
-        # cv2.line(image, (0, bottom_most_y), (width - 1, bottom_most_y), COLOR_YELLOW, LINE_THICKNESS)
-        print(f'Drawing horizontal line at Y: {bottom_most_y}')
-        return (green_line_x, bottom_most_y)
+        return (last_x_line, bottom_most_y)
 
 
 def draw_indicators(
-    image,
     binary_mask,
     intersection_coordinates,
+    output_image_path,
     draw_horizontal_lines = False,
     draw_vertical_lines = False,
     draw_intersection = False,
     draw_start_pnt = False,
     draw_end_pnt = False,
 ):  
-    new_image = image.copy()
+    new_image = cv2.cvtColor(binary_mask,cv2.COLOR_GRAY2RGB)
     height, width, _ = new_image.shape
 
     for idx, (x, y) in enumerate(intersection_coordinates):
@@ -173,6 +161,7 @@ def draw_indicators(
         if draw_end_pnt and idx == len(intersection_coordinates) - 1:
             cv2.circle(new_image, (x, y), radius=5, color=COLOR_END, thickness=-1)
     
+    cv2.imwrite(output_image_path, new_image)
     return new_image
 
 
@@ -213,47 +202,50 @@ def plot_kaplan_meier(df):
     plt.show()
 
 
-
-image, gray_image = read_images(INPUT_IMAGE)
-binary_map = get_binary_map(gray_image)
-
-horizontal_lines = get_horizontal_lines(image, binary_map, LINE_THICKNESS, LINE_SHIFT, MIN_DISTANCE, MIN_LINE_LENGTH, COLOR_HORIZONTAL)
-vertical_lines = get_vertical_lines(image, binary_map, LINE_THICKNESS, LINE_SHIFT, MIN_DISTANCE, MIN_LINE_LENGTH, COLOR_VERTICAL)
+def get_single_graf_df(binary_mask, output_image_path):
+    horizontal_lines = get_horizontal_lines(binary_map)
+    vertical_lines = get_vertical_lines(binary_map)
+    intersection_coordinates = get_intersections(horizontal_lines, vertical_lines)
 
 
-intersection_coordinates = mark_intersections(image, horizontal_lines, vertical_lines, COLOR_INTERSECTION)
+    # Get start point
+    if horizontal_lines:
+        first_y_line = horizontal_lines[0]  # Get the first horizontal blue line
+        start = get_most_top_left_white_pixel(binary_map, first_y_line)
+        intersection_coordinates.insert(0, start)
 
-# Draw vertical line at the leftmost white pixel of the first blue line
-if horizontal_lines:
-    first_blue_line_y = horizontal_lines[0]  # Get the first horizontal blue line
-    start = draw_vertical_line_at_leftmost_white(image, binary_map, first_blue_line_y)
-
-if vertical_lines:
-    last_green_line_x = vertical_lines[-1]  # Get the last vertical green line
-    end = draw_horizontal_line_at_bottom_most_white(image, binary_map, last_green_line_x)
-
-intersection_coordinates.insert(0, start)
-intersection_coordinates.append(end)
-
-print(intersection_coordinates)
+    # Get end point
+    if vertical_lines:
+        last_x_line = vertical_lines[-1]  # Get the last vertical green line
+        end = get_most_bottom_right_white_pixel(binary_map, last_x_line)
+        intersection_coordinates.append(end)
 
 
-new_image = draw_indicators(
-    image, 
-    binary_map, 
-    intersection_coordinates, 
-    draw_horizontal_lines=True,
-    draw_vertical_lines=True,
-    draw_intersection=True,
-    draw_start_pnt=True,
-    draw_end_pnt=True,
-)
+    # Draw Debug Lines
+    draw_indicators(
+        binary_map, 
+        intersection_coordinates,
+        output_image_path,
+        draw_horizontal_lines=True,
+        draw_vertical_lines=True,
+        draw_intersection=True,
+        draw_start_pnt=True,
+        draw_end_pnt=True,
+    )
 
-df = get_km_df_from_intersections(intersection_coordinates)
-df.to_csv('kaplan_meier_data.csv', index=False)
+    # Create df
+    df = get_km_df_from_intersections(intersection_coordinates)
+    df.to_csv('kaplan_meier_data.csv', index=False)
 
-plot_kaplan_meier(df)
+    plot_kaplan_meier(df)
 
-# Save or display the result
-cv2.imwrite(OUTPUT_IMAGE, new_image)
+
+if __name__ == "__main__":
+    input_image_path = 'demo/image.png'
+    output_image_path = 'demo/output_image_with_intersections.png'
+
+    image, gray_image = read_images(input_image_path)
+    binary_map = get_binary_map(gray_image)
+
+    get_single_graf_df(binary_map, output_image_path)
 
